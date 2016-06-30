6d01a1ccb868962a6ddf84b97d5476a0ce83877c
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-06-30 13:59:13 -0400

    use the json library

--- server.py
+++ server.py
@@ -1,20 +1,23 @@
 #!/usr/local/bin/python3
 import http.server
+import json
 import socketserver
 
 PORT = 8000
-SIFT = b"https://siftnlp.keatext.com"
-#SIFT = b"http://localhost:3000"
+SIFT = "https://siftnlp.keatext.com"
+#SIFT = "http://localhost:3000"
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
   def do_POST(self):
     if self.path == "/generate-temp-sift-url":
-      token = b"mytoken"
-      url = SIFT + b"/partners/ExamplePartner/login?token=" + token
+      token = "mytoken"
+      url = SIFT + "/partners/ExamplePartner/login?token=" + token
+      json_response = {"url": url}
+      binary_response = bytes(json.dumps(json_response), "utf8")
       
       self.send_response(200)
       self.end_headers()
-      self.wfile.write(b'{"url": "%s"}' % url)
+      self.wfile.write(binary_response)
     else:
       self.send_error(404)
 
