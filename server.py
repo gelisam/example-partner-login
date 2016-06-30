49fbd0e2b27d3e2464089a4def9c389f3f2bd512
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-06-30 13:43:37 -0400

    generate the URL on the server

--- server.py
+++ server.py
@@ -3,15 +3,18 @@ import http.server
 import socketserver
 
 PORT = 8000
+SIFT = b"https://siftnlp.keatext.com"
+#SIFT = b"http://localhost:3000"
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
   def do_POST(self):
-    if self.path == "/generate-temp-sift-token":
+    if self.path == "/generate-temp-sift-url":
       token = b"mytoken"
+      url = SIFT + b"/partners/ExamplePartner/login?token=" + token
       
       self.send_response(200)
       self.end_headers()
-      self.wfile.write(b'{"token": "%s"}' % token)
+      self.wfile.write(b'{"url": "%s"}' % url)
     else:
       self.send_error(404)
 
