8d2f3b88021789b81cd8ee4401354b0eb7d94129
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-07-07 10:36:28 -0400

    switch to the new URLs

--- server.py
+++ server.py
@@ -9,19 +9,21 @@ PORT = 8000
 SIFT = "https://siftnlp.keatext.com"
 #SIFT = "http://localhost:3000"
 
-PARTNER_ID = "example-partner"
 PARTNER_USERNAME = b"maelle.blanchette@keatext.com"
 PARTNER_PASSWORD = b"mypassword"
 
 
-def request_token(user_id):
-  url = "%s/partners/%s/users/%s/login_token" % (SIFT, PARTNER_ID, user_id)
-  request = urllib.request.Request(url, b"") # the b"" makes this a POST request
+def request_token(user_email):
+  url = "%s/api/v1/partner/login_token" % SIFT
+  json_payload = {"email": user_email}
+  binary_payload = json.dumps(json_payload).encode("utf8")
+  request = urllib.request.Request(url, binary_payload)
   
   # this base64 rigmarole is the "basic auth" standard.
   auth_string = PARTNER_USERNAME + b":" + PARTNER_PASSWORD
   auth_header = b"Basic " + base64.encodestring(auth_string).rstrip()
   request.add_header("Authorization", auth_header)
+  request.add_header("Content-Type", "application/json")
   
   with urllib.request.urlopen(request) as rfile:
     # json_response = {"token": token}
@@ -32,11 +34,11 @@ def request_token(user_id):
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
   def do_POST(self):
     if self.path == "/generate-temp-sift-url":
-      # ExamplePartner only has a single user, "lucille", so we hardcode it here
-      # instead of bothering to maintain an HTTP session.
-      token = request_token("lucille")
+      # ExamplePartner only has a single user, "Lucille", so we hardcode her email
+      # here instead of bothering to maintain an HTTP session.
+      token = request_token("lucille.blanchette@keatext.com")
       
-      url = "%s/partners/ExamplePartner/login?token=%s" % (SIFT, token)
+      url = "%s/partner-login?token=%s" % (SIFT, token)
       json_response = {"url": url}
       binary_response = json.dumps(json_response).encode("utf8")
       
