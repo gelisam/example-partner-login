7ef7420bcd8c40a93e8b883cbb30a73b36a94d6f
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-09-21 16:22:36 -0400

    fix the URL

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import socketserver
 import urllib.request
 
 PORT = 8000
-SIFT = "https://app.keatext.com"
+SIFT = "https://app.keatext.ai"
 #SIFT = "http://localhost:3000"
 
 PARTNER_USERNAME = b"maelle.blanchette@keatext.com"
